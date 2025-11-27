1e5a8fea400b06d9d9c7c9dcef1b460dbb88378e
Andi Rifqi Aunur Rahman <andikah3954g@gmail.com>
2025-11-27 05:08:03 +0000

    FirebaseError: Firebase: No Firebase App '[DEFAULT]' has been created -

--- src/app/shared/main-layout.tsx
+++ src/app/shared/main-layout.tsx
@@ -1,26 +1,7 @@
-'use client';
-
-import { useUser } from "@/firebase";
-import { redirect } from "next/navigation";
 import { Header } from "./header";
 import { Footer } from "./footer";
-import { Loader2 } from "lucide-react";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-  const { user, isUserLoading } = useUser();
-
-  if (isUserLoading) {
-    return (
-      <div className="flex h-screen w-full items-center justify-center">
-        <Loader2 className="h-8 w-8 animate-spin" />
-      </div>
-    );
-  }
-
-  if (!user) {
-    redirect("/login");
-  }
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
